a8da295680016a248144be2547f5df0505c91a5a
Awekonti <awekonti.by@gmail.com>
2025-06-29 16:49:20 +0800

    add dataset class and utils

---
+++ data_utils.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+from torch.utils import Dataset, DataLoader, random_split
+
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizer.models import WordLevel
+from tokenizer.trainers import WordLevelTrainer
+from tokenizer.pre_tokenizers import Whitespace
+
+from typing import Dict, Any
+from pathlib import Path
+
+from dataset import BilingualDataset, causual_mask
+
+HG_DATASET_NAME = "Helsinki-NLP/opus_books"
+
+
+def get_all_sentences(dataset, language: str):
+    """
+    Extract all sentences from the dataset for the specified language.
+    """
+    for item in dataset:
+        yield item["translation"][language]
+
+
+def build_tokenzier(config: Dict, dataset, language: str):
+    tokenizer_path = Path(config["tokenizer_path"].format(language))
+
+    if not Path.exists(tokenizer_path):
+        print(f"Building tokenizer for {language}... ({tokenizer_path})")
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+
+        trainer = WordLevelTrainer(
+            vocab_size=config["vocab_size"],
+            special_tokens=["[UNK]", "[PAD]}", "[SOS]", "[EOS]"],
+            min_frequency=config["min_frequency"],
+        )
+
+        tokenizer.train_from_iterator(
+            get_all_sentences(dataset, language), trainer=trainer
+        )
+        tokenizer.save(str(tokenizer_path))
+    else:
+        print(f"Loading existing tokenizer for {language}... ({tokenizer_path})")
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+
+    return tokenizer
+
+
+def load_dataset_and_tokenizer(config: Dict, language: str):
+    """
+    Load the dataset and tokenizer for the specified language.
+    """
+
+    def calcualate_max_seq_len(dataset, tokenizer):
+        return max(
+            len(tokenizer.encode(item["translation"][language]).ids) for item in dataset
+        )
+
+    dataset = load_dataset(
+        HG_DATASET_NAME,
+        f"{config['language_src']}-{config['language_tgt']}",
+        split="train",
+    )
+
+    # Build or load the tokenizer for the specified languag
+    tokenizer_src = build_tokenzier(config, dataset, config["language_src"])
+    tokenizer_tgt = build_tokenzier(config, dataset, config["language_tgt"])
+
+    train_size = int(config["train_size"] * len(dataset))
+    val_size = len(dataset) - train_size
+    train_set_raw, val_set_raw = random_split(dataset, (train_size, val_size))
+
+    trains_set = BilingualDataset(
+        train_set_raw,
+        config["language_src"],
+        config["language_tgt"],
+        tokenizer_src,
+        tokenizer_tgt,
+        config["seq_len"],
+    )
+
+    val_set = BilingualDataset(
+        val_set_raw,
+        config["language_src"],
+        config["language_tgt"],
+        tokenizer_src,
+        tokenizer_tgt,
+        config["seq_len"],
+    )
+
+    print(
+        f"Max sequence length for {config['language_src']}: {calcualate_max_seq_len(train_set_raw, tokenizer_src)}"
+    )
+    print(
+        f"Max sequence length for {config['language_tgt']}: {calcualate_max_seq_len(train_set_raw, tokenizer_tgt)}"
+    )
+
+    train_dataloader = DataLoader(
+        trains_set,
+        batch_size=config["batch_size"],
+        shuffle=True,
+    )
+    val_dataloader = DataLoader(
+        val_set,
+        batch_size=1,
+        shuffle=False,
+    )
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
---
+++ dataset.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
+from typing import Dict, Any
+
+
+def causual_mask(seq_len: int) -> torch.Tensor:
+    """
+    Create a causal mask for the given sequence length.
+    The mask is used to prevent attending to future tokens in the sequence.
+    """
+    mask = torch.tril(torch.ones(1, seq_len, seq_len), diagonal=1).type(torch.int)
+    return mask
+
+
+class BilingualDataset(Dataset):
+    def __init__(
+        self,
+        dataset,
+        src_language: str,
+        tgt_language: str,
+        src_tokenizer,
+        tgt_tokenizer,
+        seq_len: int,
+    ):
+        super.__init__()
+        self.dataset = dataset
+        self.src_language = src_language
+        self.tgt_language = tgt_language
+        self.src_tokenizer = src_tokenizer
+        self.tgt_tokenizer = tgt_tokenizer
+        self.seq_len = seq_len
+
+        self.sos_token = torch.Tensor(
+            [src_tokenizer.token_to_id("[SOS]")], dtype=torch.int64
+        )
+        self.eos_token = torch.Tensor(
+            [src_tokenizer.token_to_id("[EOS]")], dtype=torch.int64
+        )
+        self.pad_token = torch.Tensor(
+            [src_tokenizer.token_to_id("[PAD]")], dtype=torch.int64
+        )
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx: Any) -> Any:
+        src_target_pair = self.dataset[idx]
+        src_text = src_target_pair["translation"][self.src_language]
+        tgt_text = src_target_pair["translation"][self.tgt_language]
+
+        encoder_input_tokens = self.src_tokenizer.encode(src_text).ids
+        decoder_input_tokens = self.tgt_tokenizer.encode(tgt_text).ids
+
+        num_pad_tokens_encoder = self.seq_len - len(encoder_input_tokens) - 2
+        num_pad_tokens_decoder = self.seq_len - len(decoder_input_tokens) - 1
+
+        if num_pad_tokens_encoder < 0 or num_pad_tokens_decoder < 0:
+            raise ValueError("Sequence length is too long.")
+
+        # Add SOS and EOS tokens to the encoder input
+        # and pad the sequence to the required length
+        encoder_input_tokens = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(encoder_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor(
+                    [self.pad_token] * num_pad_tokens_encoder, dtype=torch.int64
+                ),
+            ]
+        )
+
+        # Add SOS token to the decoder input
+        # and pad the sequence to the required length
+        encoder_input_tokens = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(decoder_input_tokens, dtype=torch.int64),
+                torch.tensor(
+                    [self.pad_token] * num_pad_tokens_decoder, dtype=torch.int64
+                ),
+            ]
+        )
+
+        # Create the label tensor with EOS token and padding
+        # The label is the decoder input tokens with EOS at the end
+        # and padded to the required length
+        label = torch.cat(
+            [
+                torch.tensor(decoder_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor(
+                    [self.pad_token] * num_pad_tokens_decoder, dtype=torch.int64
+                ),
+            ]
+        )
+
+        assert (
+            len(encoder_input_tokens) == self.seq_len
+        ), "Encoder input length mismatch."
+        assert (
+            len(decoder_input_tokens) == self.seq_len
+        ), "Decoder input length mismatch."
+        assert len(label) == self.seq_len, "Label length mismatch."
+
+        encoder_mask = encoder_input_tokens != self.pad_token
+        encoder_mask = encoder_input_tokens.unsqueeze(0).unsqueeze(0).int()
+
+        decoder_mask = decoder_input_tokens != self.pad_token
+        decoder_mask = decoder_mask.unsqueeze(0).unsqueeze(0).int() & causual_mask(
+            len(decoder_input_tokens)
+        )
+
+        return {
+            "encoder_input": encoder_input_tokens,  # (seq_len)
+            "decoder_input": decoder_input_tokens,  # (seq_len)
+            "encoder_mask": encoder_mask,  # (1 - batch, 1 - d_model, seq_len)
+            "decoder_mask": decoder_mask,  # (1 - batch, 1 - d_model, seq_len)
+            "label": label,  # (seq_len)
+            "src_text": src_text,
+            "tgt_text": tgt_text,
+        }
